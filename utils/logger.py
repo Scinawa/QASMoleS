49bf82a45292da0f1172a2be8f545a6ab52e1d35
Scinawa <2940017+Scinawa@users.noreply.github.com>
2024-06-27 19:58:49 +0800

    Make logger work again.

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,26 @@
 import tensorflow as tf
 
 
-class Logger(object):
+# class Logger(object):
 
+#     def __init__(self, log_dir):
+#         """Create a summary writer logging to log_dir."""
+#         #self.writer = tf.compat.v1.summary.FileWriter(log_dir)
+#         self.writer = tf.summary.create_file_writer(log_dir)
+
+
+#     def scalar_summary(self, tag, value, step):
+#         """Log a scalar variable."""
+#         summary = tf.compat.v1.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+#         self.writer.add_summary(summary, step)
+
+
+
+class Logger:
     def __init__(self, log_dir):
-        """Create a summary writer logging to log_dir."""
-        self.writer = tf.compat.v1.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        """Log a scalar variable."""
-        summary = tf.compat.v1.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(name=tag, data=value, step=step)
+            self.writer.flush()
